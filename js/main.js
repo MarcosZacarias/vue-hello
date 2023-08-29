db32875ae1e7e9c306415ceeebeb22b9c31f349f
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-08-29 16:16:29 +0200

    add: function print image

--- js/main.js
+++ js/main.js
@@ -4,7 +4,23 @@ createApp({
   data() {
     return {
       message: "Hello World",
-      srcImg: "./img/01.webp",
+      numberImg: "",
+      srcImg: "",
+      imgDisplay: "d-none",
     };
+  },
+
+  methods: {
+    printImage() {
+      if (this.numberImg <= 5 && this.numberImg >= 1) {
+        this.imgDisplay = "d-inline-block";
+        console.log(this.numberImg);
+        let srcImage = "./img/0" + this.numberImg + ".webp";
+        console.log(srcImage);
+        this.srcImg = srcImage;
+      } else {
+        alert("Inserire un numero da 1 a 5");
+      }
+    },
   },
 }).mount("#app");
